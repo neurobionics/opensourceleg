67807e9ca3d6f21ad53c12d2eeb7104a69172b33
Senthur <imsenthur@gmail.com>
2022-04-15 14:49:39 -0400

    Open-Source Leg! :tada:

---
+++ opensourceleg/__init__.py
@@ -0,0 +1,15 @@
+# type: ignore[attr-defined]
+"""An open-source software library for numerical computation, data acquisition, and control of lower-limb robotic prosthesis."""
+
+import sys
+from importlib import metadata as importlib_metadata
+
+
+def get_version() -> str:
+    try:
+        return importlib_metadata.version(__name__)
+    except importlib_metadata.PackageNotFoundError:  # pragma: no cover
+        return "unknown"
+
+
+version: str = get_version()
