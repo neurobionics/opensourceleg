9a5ed80d5c4479daffa6906eb419cff5023ec3cf
Senthur <imsenthur@gmail.com>
2023-06-07 14:53:54 -0400

    Added loadcell tutorial.

---
+++ tutorials/adding_joints.py
@@ -0,0 +1,5 @@
+from opensourceleg.osl import OpenSourceLeg
+
+osl = OpenSourceLeg(frequency=200, file_name="getting_started.log")
+osl.add_joint(name="knee", gear_ratio=41.99, has_loadcell=False)
+osl.add_joint(name="ankle", gear_ratio=41.99, has_loadcell=False)
