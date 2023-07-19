2a8d5e791dafe1a36ab1499e90f388680300a360
jderosia <jdero@umich.edu>
2023-07-19 10:34:37 -0400

    Fixed typo in impedance mode tutorial.

--- tutorials/impedance_mode.py
+++ tutorials/impedance_mode.py
@@ -12,12 +12,12 @@ test_damping_value = 20
 set_point = 70
 
 with osl:
-    
+
     osl.knee.set_mode("impedance")
     osl.knee.set_joint_impedance(K=test_stiffness_value, B=test_damping_value)
-    
+
     for t in osl.clock:
-        
+
         osl.knee.set_motor_position(osl.knee.motor_position + set_point)
         osl.log.info(osl.knee.motor_position)
         osl.update()
