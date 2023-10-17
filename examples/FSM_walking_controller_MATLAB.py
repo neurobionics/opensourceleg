b4e52af8f449dd67ea12d60f0ad2327f971fb88b
Kevin Best <tkevinbest@gmail.com>
2023-10-17 14:56:51 -0400

    Added input and output parsing to CompiledController class. Updates to example code.

--- examples/FSM_walking_controller_MATLAB.py
+++ examples/FSM_walking_controller_MATLAB.py
@@ -9,48 +9,66 @@ October 9, 2023
 """
 
 # Imports
+import os
 import sys
-import ctypes
-sys.path.append("../")
+import inspect
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir) 
 from opensourceleg.osl import OpenSourceLeg
+import opensourceleg.control as control
 
 # Initialization
 osl = OpenSourceLeg(frequency=200)
-osl.add_joint('knee', 9*83/18)
-osl.add_joint('ankle',9*83/18)
-osl.add_loadcell(joint=osl.knee)
+use_offline_mode = True
+osl.add_joint('knee', gear_ratio=9*83/18, port=None, use_offline_mode=True)
+osl.add_joint('ankle',gear_ratio=9*83/18, port=None, use_offline_mode=True)
+osl.add_loadcell(joint=osl.knee, use_offline_mode=True)
 osl.units["position"] = "deg"  # type: ignore
 osl.units["velocity"] = "deg/s"  # type: ignore
 
-# Define external library control function interface
-sensors = osl.control.default_sensors
+# Instantiate a compiled controller wrapper object
+controller = control.CompiledController(main_function_name = 'FSM_Walking_Controller.so',
+                                        controller_path = currentdir, 
+                                        initialization_function_name = None,
+                                        cleanup_function_name = None)
 
-# Define custom types for parameters and outputsas list of tuples
-impedance_param_type = [('stiffness',ctypes.c_double),
-                        ('damping',ctypes.c_double),
-                        ('eq_angle',ctypes.c_double)]
-joint_impedances = [('early_stance_impedance', impedance_param_type), 
-                    ('late_stance_impedance', impedance_param_type), 
-                    ('early_swing_impedance', impedance_param_type), 
-                    ('late_swing_impedance', impedance_param_type)]
-params = [('knee_impedance', joint_impedances), ('ankle_impedance', joint_impedances)]
-outputs = [('knee_impedance', impedance_param_type), ('ankle_impedance', impedance_param_type)]
 
-# Instantiate controller
-controller = osl.control.load_library_controller('MATLAB_FMS_walking_controller.so',
-                                                 sensors, params, outputs)
+# Define custom types for parameters and outputs as list of tuples
+controller.Define_Type('impedance_param_type',[('stiffness',controller.types.c_double),
+                        ('damping',controller.types.c_double),
+                        ('eq_angle',controller.types.c_double)])
+controller.Define_Type('joint_impedances', [('early_stance', controller.types.impedance_param_type), 
+                    ('late_stance', controller.types.impedance_param_type), 
+                    ('early_swing', controller.types.impedance_param_type), 
+                    ('late_swing', controller.types.impedance_param_type)])
+controller.Define_Type('UserParameters', [('knee_impedance', controller.types.joint_impedances),('ankle_impedance', controller.types.joint_impedances)])
+controller.Define_Type('sensors', controller.DEFAULT_SENSOR_LIST)
+
+controller.Define_Inputs([('parameters',controller.types.UserParameters), ('sensors',controller.types.sensors)])
+controller.Define_Outputs([('knee_impedance', controller.types.impedance_param_type), ('ankle_impedance', controller.types.impedance_param_type)])
+
+# Populate Controller inputs as needed
+controller.inputs.parameters.knee_impedance.early_stance.stiffness = 5
+
+# Configure state machine
 
 with osl:
     osl.home()
     osl.calibrate_loadcell()
+    osl.knee.set_mode('impedance')
+    osl.ankle.set_mode('impedance')
 
     # Main Loop
     for t in osl.clock:
         # Read from the hardware
         osl.update()
 
+        # sensors = 
+
         # Call the controller
-        outputs = controller.run(sensors, params)
+        # outputs = controller.run(sensors, params)
 
         # Write to the hardware
         osl.knee.set_joint_impedance(K = outputs.knee_impedance.stiffness,
--- opensourceleg/control.py
+++ opensourceleg/control.py
@@ -9,7 +9,6 @@ import numpy.ctypeslib as ctl
 
 from opensourceleg.utilities import get_ctype_args
 
-
 @dataclass
 class ControlModes:
     """
@@ -117,32 +116,72 @@ class Sensors(ctypes.Structure):
     ]
 
 
-class Controller:
+class CompiledController:
     """
-    Controller class to load compiled controllers.
+    Controller class to handle using compiled controllers.
     """
 
-    def __init__(self, frequency: float) -> None:
+    def __init__(self, main_function_name, controller_path, initialization_function_name, cleanup_function_name) -> None:
         """
         Initialize the Controller class.
 
         Parameters
         ----------
-        frequency : float
-            Frequency of the controller
+        main_function_name : string
+            Name of the main function to call within the library
+        controller_path : string
+            Full path to the .so object. 
         """
-        self._frequency = frequency
-        self._lib: ctypes.CDLL = None
+        # Load functions
+        self.lib_cleanup = self._load_function(cleanup_function_name, controller_path)
+        self.lib = self._load_function(main_function_name, controller_path)
+        self.lib_init = self._load_function(initialization_function_name, controller_path)
+        
+        # Call init function if there is one
+        if not self.lib_init == None:
+            self.lib_init()
 
-        self._sensors = Sensors()
+        # Alias the ctypes class as member of this class
+        self.types = ctypes
 
-    def load(self, controller_name, controller_path) -> ctypes.CDLL:
-        self.lib = ctl.load_library(controller_name, controller_path)
+        # Define default sensor list
+        self.DEFAULT_SENSOR_LIST = [('knee_angle',self.types.c_double),
+                                    ('ankle_angle',self.types.c_double)]
+        
+        # Initialize input and output lists
+        self._input_type = None
+        self.inputs = None
+        self._output_type = None
+        self.outputs = None
 
+    def __del__(self):
+        if not self.lib_cleanup == None:
+            self.lib_cleanup()
+
+    def _load_function(self, controller_name, controller_path) -> ctypes.CDLL:
+        if controller_name == None:
+            return None
+        else:
+            return ctl.load_library(controller_name, controller_path)
+
+    def Define_Inputs(self, input_list):
+        self._input_type = self.Define_Type('inputs', input_list)
+        self.inputs = self._input_type()
+
+    def Define_Outputs(self, output_list): 
+        self._output_type = self.Define_Type('outputs',output_list)
+        self.outputs = self._output_type()
+
+    def Define_Type(self, type_name, parameter_list):
+        slots = []
+        for param in parameter_list:
+                slots.append(param[0])
+        class ctypes_structure(ctypes.Structure):
+            _fields_ = parameter_list
+            __slots__ = slots
+        setattr(self.types, type_name, ctypes_structure)
+        return getattr(self.types, type_name)
 
 if __name__ == "__main__":
 
-    from opensourceleg.control import Controller
-
-    hkic_controller = Controller(200)
-    hkic_controller.load("hkic_controller.so", "../")
+    pass
