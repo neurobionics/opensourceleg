7f0ede39268c828e54d56b3183c13349f98ed1cf
Kevin Best <tkevinbest@gmail.com>
2023-10-19 08:50:33 -0400

    Ran linting and codestyle.

--- examples/FSM_walking_controller_MATLAB.py
+++ examples/FSM_walking_controller_MATLAB.py
@@ -8,26 +8,31 @@ University of Michigan
 October 9, 2023
 """
 
+import inspect
+
 # Imports
 import os
 import sys
-import inspect
+
 import numpy as np
 
-# Path fixes to handle running directly from cloned repo instead of pip installed. 
+# Path fixes to handle running directly from cloned repo instead of pip installed.
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
-sys.path.insert(0, parentdir) 
+sys.path.insert(0, parentdir)
+
+import opensourceleg.control as control
 
 # Import OSL packages
 from opensourceleg.osl import OpenSourceLeg
-import opensourceleg.control as control
 
 # Initialization
 osl = OpenSourceLeg(frequency=200)
 use_offline_mode = True
-osl.add_joint('knee', gear_ratio=9*83/18, port=None, offline_mode=use_offline_mode)
-osl.add_joint('ankle',gear_ratio=9*83/18, port='todo', offline_mode=use_offline_mode)
+osl.add_joint("knee", gear_ratio=9 * 83 / 18, port=None, offline_mode=use_offline_mode)
+osl.add_joint(
+    "ankle", gear_ratio=9 * 83 / 18, port="todo", offline_mode=use_offline_mode
+)
 LOADCELL_MATRIX = np.array(
     [
         (-38.72600, -1817.74700, 9.84900, 43.37400, -44.54000, 1824.67000),
@@ -38,49 +43,77 @@ LOADCELL_MATRIX = np.array(
         (-0.65100, -28.28700, 0.02200, -25.23000, 0.47300, -27.3070),
     ]
 )
-osl.add_loadcell(joint=osl.knee, offline_mode=use_offline_mode, loadcell_matrix=LOADCELL_MATRIX)
+osl.add_loadcell(
+    joint=osl.knee, offline_mode=use_offline_mode, loadcell_matrix=LOADCELL_MATRIX
+)
 
 # Instantiate a compiled controller wrapper object
-controller = control.CompiledController(library_name = 'FSM_WalkingController',
-                                        library_path = currentdir, 
-                                        main_function_name = 'FSMController',
-                                        initialization_function_name = 'FSMController_initialize',
-                                        cleanup_function_name = 'FSMController_terminate')
+controller = control.CompiledController(
+    library_name="FSM_WalkingController",
+    library_path=currentdir,
+    main_function_name="FSMController",
+    initialization_function_name="FSMController_initialize",
+    cleanup_function_name="FSMController_terminate",
+)
 
 # Define custom types for parameters and outputs as list of tuples
-controller.Define_Type('impedance_param_type',
-                       [('stiffness',controller.types.c_double),
-                        ('damping',controller.types.c_double),
-                        ('eq_angle',controller.types.c_double)])
-controller.Define_Type('joint_impedance_set', 
-                       [('early_stance', controller.types.impedance_param_type), 
-                        ('late_stance', controller.types.impedance_param_type), 
-                        ('early_swing', controller.types.impedance_param_type), 
-                        ('late_swing', controller.types.impedance_param_type)])
-controller.Define_Type('transition_parameters',
-                       [('min_time_in_state', controller.types.c_double),
-                        ('loadLStance', controller.types.c_double),
-                        ('ankleThetaEstanceToLstance', controller.types.c_double),
-                        ('kneeThetaESwingToLSwing', controller.types.c_double),
-                        ('kneeDthetaESwingToLSwing', controller.types.c_double),
-                        ('loadESwing', controller.types.c_double),
-                        ('loadEStance', controller.types.c_double),
-                        ('kneeThetaLSwingToEStance', controller.types.c_double)])
-controller.Define_Type('UserParameters', 
-                       [('body_weight',controller.types.c_double),
-                        ('knee_impedance', controller.types.joint_impedance_set),
-                        ('ankle_impedance', controller.types.joint_impedance_set), 
-                        ('transition_parameters',controller.types.transition_parameters)])
-controller.Define_Type('sensors', controller.DEFAULT_SENSOR_LIST)
+controller.Define_Type(
+    "impedance_param_type",
+    [
+        ("stiffness", controller.types.c_double),
+        ("damping", controller.types.c_double),
+        ("eq_angle", controller.types.c_double),
+    ],
+)
+controller.Define_Type(
+    "joint_impedance_set",
+    [
+        ("early_stance", controller.types.impedance_param_type),
+        ("late_stance", controller.types.impedance_param_type),
+        ("early_swing", controller.types.impedance_param_type),
+        ("late_swing", controller.types.impedance_param_type),
+    ],
+)
+controller.Define_Type(
+    "transition_parameters",
+    [
+        ("min_time_in_state", controller.types.c_double),
+        ("loadLStance", controller.types.c_double),
+        ("ankleThetaEstanceToLstance", controller.types.c_double),
+        ("kneeThetaESwingToLSwing", controller.types.c_double),
+        ("kneeDthetaESwingToLSwing", controller.types.c_double),
+        ("loadESwing", controller.types.c_double),
+        ("loadEStance", controller.types.c_double),
+        ("kneeThetaLSwingToEStance", controller.types.c_double),
+    ],
+)
+controller.Define_Type(
+    "UserParameters",
+    [
+        ("body_weight", controller.types.c_double),
+        ("knee_impedance", controller.types.joint_impedance_set),
+        ("ankle_impedance", controller.types.joint_impedance_set),
+        ("transition_parameters", controller.types.transition_parameters),
+    ],
+)
+controller.Define_Type("sensors", controller.DEFAULT_SENSOR_LIST)
 
 # Define the function interface of the external library
-controller.Define_Inputs([('parameters',controller.types.UserParameters), 
-                          ('sensors',controller.types.sensors), 
-                          ('time',controller.types.c_double)])
-controller.Define_Outputs([('current_state', controller.types.c_uint8),
-                           ('time_in_current_state', controller.types.c_double),
-                           ('knee_impedance', controller.types.impedance_param_type), 
-                           ('ankle_impedance', controller.types.impedance_param_type)])
+controller.Define_Inputs(
+    [
+        ("parameters", controller.types.UserParameters),
+        ("sensors", controller.types.sensors),
+        ("time", controller.types.c_double),
+    ]
+)
+controller.Define_Outputs(
+    [
+        ("current_state", controller.types.c_uint8),
+        ("time_in_current_state", controller.types.c_double),
+        ("knee_impedance", controller.types.impedance_param_type),
+        ("ankle_impedance", controller.types.impedance_param_type),
+    ]
+)
 
 # Populate Controller inputs as needed
 controller.inputs.parameters.knee_impedance.early_stance.stiffness = 5
@@ -100,13 +133,15 @@ with osl:
     for t in osl.clock:
         # Read from the hardware and update the inputs object
         osl.update()
-        
+
         # TODO: Make this automated for standard inputs list
-        controller.inputs.sensors.knee_angle = osl.knee.output_position # Default is in radians
+        controller.inputs.sensors.knee_angle = (
+            osl.knee.output_position
+        )  # Default is in radians
         controller.inputs.sensors.ankle_angle = osl.ankle.output_position
         controller.inputs.sensors.knee_velocity = osl.knee.output_velocity
         controller.inputs.sensors.ankle_velocity = osl.ankle.output_velocity
-        controller.inputs.sensors.Fz = osl.loadcell.fz # Newtons?
+        controller.inputs.sensors.Fz = osl.loadcell.fz  # Newtons?
 
         # Update any control inputs that change every loop
         outputs = controller.inputs.time = t
@@ -115,9 +150,11 @@ with osl:
         outputs = controller.run()
 
         # Write to the hardware
-        osl.knee.set_joint_impedance(K = outputs.knee_impedance.stiffness,
-                                     B = outputs.knee_impedance.damping)
-        osl.knee.set_output_position(position = outputs.knee_impedance.eq_angle)
-        osl.ankle.set_joint_impedance(K = outputs.ankle_impedance.stiffness,
-                                      B = outputs.ankle_impedance.damping)
-        osl.ankle.set_output_position(position = outputs.ankle_impedance.eq_angle)
+        osl.knee.set_joint_impedance(
+            K=outputs.knee_impedance.stiffness, B=outputs.knee_impedance.damping
+        )
+        osl.knee.set_output_position(position=outputs.knee_impedance.eq_angle)
+        osl.ankle.set_joint_impedance(
+            K=outputs.ankle_impedance.stiffness, B=outputs.ankle_impedance.damping
+        )
+        osl.ankle.set_output_position(position=outputs.ankle_impedance.eq_angle)
--- opensourceleg/control.py
+++ opensourceleg/control.py
@@ -46,15 +46,16 @@ class Gains:
     def __repr__(self) -> str:
         return f"kp={self.kp}, ki={self.ki}, kd={self.kd}, K={self.K}, B={self.B}, ff={self.ff}"
 
+
 class CompiledController:
     """
     Controller class to handle using compiled controllers.
     This class expects that your function has the form:
         myFunction(*inputs, *outputs)
-    where *inputs is a pointer to an inputs structure and 
-    *outputs is a pointer to an outputs structure. 
-    You can define these input and output structures however you please. 
-    See examples folder of repo for examples. 
+    where *inputs is a pointer to an inputs structure and
+    *outputs is a pointer to an outputs structure.
+    You can define these input and output structures however you please.
+    See examples folder of repo for examples.
 
     Kevin Best, Senthur Raj Ayyappan
     Neurobionics Lab
@@ -62,7 +63,15 @@ class CompiledController:
     University of Michigan
     October 2023
     """
-    def __init__(self, library_name , library_path, main_function_name, initialization_function_name, cleanup_function_name) -> None:
+
+    def __init__(
+        self,
+        library_name,
+        library_path,
+        main_function_name,
+        initialization_function_name,
+        cleanup_function_name,
+    ) -> None:
         """
         Initialize the Controller class.
 
@@ -71,26 +80,26 @@ class CompiledController:
         library_name : string
             The name of the compiled library file, without the *.so
         library_path : string
-            The path to the directory containing the library. 
+            The path to the directory containing the library.
             See examples for how to get working directory of parent script.
         main_function_name : string
-            Name of the main function to call within the library. 
+            Name of the main function to call within the library.
             This is the function that will get called via the run() method
         initialization_function_name : string
             Name of an initialization function for your library. This gets
-            called only once when the library is loaded. 
+            called only once when the library is loaded.
             If you don't have an initialization function, pass None.
         cleanup_function_name : string
             Name of a cleanup function for your library. This gets called when
-            the CompiledController class has gone out of scope and is 
-            garbage collected. Again, pass None if you don't need this functionality. 
+            the CompiledController class has gone out of scope and is
+            garbage collected. Again, pass None if you don't need this functionality.
         """
         # Load functions
         self.lib = ctl.load_library(library_name, library_path)
         self.cleanup_func = self._load_function(cleanup_function_name)
         self.main_func = self._load_function(main_function_name)
         self.init_func = self._load_function(initialization_function_name)
-        
+
         # Call init function if there is one
         if not self.init_func == None:
             self.init_func()
@@ -99,12 +108,14 @@ class CompiledController:
         self.types = ctypes
 
         # Define default sensor list
-        self.DEFAULT_SENSOR_LIST = [('knee_angle',self.types.c_double),
-                                    ('ankle_angle',self.types.c_double), 
-                                    ('knee_velocity', self.types.c_double),
-                                    ('ankle_velocity', self.types.c_double),
-                                    ('Fz', self.types.c_double)]
-        
+        self.DEFAULT_SENSOR_LIST = [
+            ("knee_angle", self.types.c_double),
+            ("ankle_angle", self.types.c_double),
+            ("knee_velocity", self.types.c_double),
+            ("ankle_velocity", self.types.c_double),
+            ("Fz", self.types.c_double),
+        ]
+
         # Initialize input and output lists
         self._input_type = None
         self.inputs = None
@@ -121,85 +132,92 @@ class CompiledController:
         else:
             return getattr(self.lib, function_name)
 
-    def Define_Inputs(self, input_list : list) -> None:
+    def Define_Inputs(self, input_list: list) -> None:
         """
-        This method defines the input structure to your function. 
-        See example folder and tutorials for help on using this method. 
+        This method defines the input structure to your function.
+        See example folder and tutorials for help on using this method.
 
         Parameters
         ------------
         input_list: Input parameters given as a list of [('field_name', field_type)...]
             field_name is a string you choose as the title of the field.
-            field_type is a type either given by a native c_types value or 
-                a custom type defined via the Define_Type() method. 
+            field_type is a type either given by a native c_types value or
+                a custom type defined via the Define_Type() method.
                 All types can be accessed as CompiledController.types.(type_name)
         """
-        self._input_type = self.Define_Type('inputs', input_list)
+        self._input_type = self.Define_Type("inputs", input_list)
         self.inputs = self._input_type()
 
-    def Define_Outputs(self, output_list : list) -> None: 
+    def Define_Outputs(self, output_list: list) -> None:
         """
-        This method defines the output structure to your function. 
-        See example folder and tutorials for help on using this method. 
+        This method defines the output structure to your function.
+        See example folder and tutorials for help on using this method.
 
         Parameters
         ------------
         output_list: Output parameters given as a list of [('field_name', field_type)...]
             field_name is a string you choose as the title of the field.
-            field_type is a type either given by a native c_types value or 
-                a custom type defined via the Define_Type() method. 
+            field_type is a type either given by a native c_types value or
+                a custom type defined via the Define_Type() method.
                 All types can be accessed as CompiledController.types.(type_name)
         """
-        self._output_type = self.Define_Type('outputs',output_list)
+        self._output_type = self.Define_Type("outputs", output_list)
         self.outputs = self._output_type()
 
-    def Define_Type(self, type_name : str, parameter_list : list) :
+    def Define_Type(self, type_name: str, parameter_list: list):
         """
         This method defines a new type to be used in the compiled controller.
-        After calling this method, the datatype with name type_name will be 
-        available in my_controller.types.type_name for use.  
-        See example folder and tutorials for help on using this method. 
+        After calling this method, the datatype with name type_name will be
+        available in my_controller.types.type_name for use.
+        See example folder and tutorials for help on using this method.
 
         Parameters
         ------------
         type_name : A string defining the name of your new datatype
         parameter_list: A list of [('field_name', field_type)...]
             field_name is a string you choose as the title of the field.
-            field_type is a type either given by a native c_types value or 
-                a custom type defined via the Define_Type() method. 
+            field_type is a type either given by a native c_types value or
+                a custom type defined via the Define_Type() method.
                 All types can be accessed as CompiledController.types.(type_name)
 
         Example Usage:
-            my_controller.DefineType('vector3D', [('x', my_controller.types.c_double), 
+            my_controller.DefineType('vector3D', [('x', my_controller.types.c_double),
                                                   ('y', my_controller.types.c_double),
                                                   ('z', my_controller.types.c_double)])
         """
         slots = []
         for param in parameter_list:
-                slots.append(param[0])
+            slots.append(param[0])
+
         class CustomStructure(ctypes.Structure):
             _fields_ = parameter_list
             __slots__ = slots
+
         setattr(self.types, type_name, CustomStructure)
         return getattr(self.types, type_name)
-    
+
     def run(self):
         """
-        This method calls the main controller function of the library. 
+        This method calls the main controller function of the library.
         Under the hood, it calls library_name.main_function_name(*inputs, *outputs),
-        where library_name and main_function_name were given in the constructor. 
+        where library_name and main_function_name were given in the constructor.
 
         Parameters -> None
 
-        Returns: 
-            The output structure as defined by the Define_Outputs() method. 
+        Returns:
+            The output structure as defined by the Define_Outputs() method.
         """
         if self.inputs is None:
-            raise ValueError('Must define input type before calling controller.run(). Use Define_Inputs() method.')
+            raise ValueError(
+                "Must define input type before calling controller.run(). Use Define_Inputs() method."
+            )
         if self.outputs is None:
-            raise ValueError('Must define output type before calling controller.run(). Use Define_Outputs() method.')
+            raise ValueError(
+                "Must define output type before calling controller.run(). Use Define_Outputs() method."
+            )
         self.main_func(ctypes.byref(self.inputs), ctypes.byref(self.outputs))
         return self.outputs
+
 
 if __name__ == "__main__":
 
