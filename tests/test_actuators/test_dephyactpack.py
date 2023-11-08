Fixed units for motor velocity.

tests/test_actuators/test_dephyactpack.py
@@ -351,7 +351,7 @@ def test_properties_nonzero(dephyactpack_patched: DephyActpack):
     assert mock_dap1.motor_position == 10 * 2 * np.pi / 16384
     assert mock_dap1.motor_encoder_counts == 10
     assert mock_dap1.joint_encoder_counts == 20
-    assert mock_dap1.motor_velocity == 10 * 2 * np.pi / 16384
+    assert mock_dap1.motor_velocity == 10 * np.pi / 180
     assert mock_dap1.motor_acceleration == 20
     assert mock_dap1.joint_position == 20 * 2 * np.pi / 16384
     assert mock_dap1.joint_velocity == 10 * 2 * np.pi / 16384

tests/test_joints/test_joint.py
@@ -540,5 +540,5 @@ def test_mockjoint_nondefaultproperties(joint_patched):
     assert jp2.is_homed == True
     assert jp2.encoder_map == None
     assert jp2.output_position == 20 * 2 * np.pi / 16384 / 50
-    assert jp2.output_velocity == 10 * 2 * np.pi / 16384 / 50
+    assert jp2.output_velocity == 10 * np.pi / 180 / 50
     assert jp2.joint_torque == 20 * 0.1133 / 1000 * 50