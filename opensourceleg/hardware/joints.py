001a2452f604d10277f0b5e4ce4cd2aa7465518f
jderosia <jdero@umich.edu>
2024-03-11 20:20:45 -0400

    Fixed style

--- opensourceleg/hardware/joints.py
+++ opensourceleg/hardware/joints.py
@@ -443,7 +443,7 @@ class MockJoint(Joint, MockDephyActpack):
         self._max_temperature: float = MAX_CASE_TEMPERATURE
 
         self._clib = MockClib()
-    
+
         if "knee" in name.lower() or "ankle" in name.lower():
             self._name: str = name
         else:
@@ -451,14 +451,13 @@ class MockJoint(Joint, MockDephyActpack):
             return
 
 
-class MockClib():
+class MockClib:
 
     def __init__() -> None:
         a: bool = False
 
     def fxIsOpen(self, val) -> bool:
         return True
-
 
 
 if __name__ == "__main__":
