bf79c7bd469db64d315442f1fc2480ca3b9a2d6d
Yuanshao Yang <yuanshao@umich.edu>
2024-05-05 00:02:49 -0400

    20240505

--- opensourceleg/hardware/actuators_Base.py
+++ opensourceleg/hardware/actuators_Base.py
@@ -1,8 +1,4 @@
-﻿"""
-New Actuator Module for Abstract Base Class
-Updated 05/2024
-"""
-
+﻿# Actuators 
 from typing import Any, Callable, Union, overload
 
 import ctypes
@@ -10,22 +6,13 @@ import os
 import time
 from ctypes import c_int
 from dataclasses import dataclass
-import numpy as np
-# Abstract Base Class Definition
-from abc import ABC, abstractmethod
 
 import flexsea.fx_enums as fxe
-
-"""
-
-
+import numpy as np
 from flexsea.device import Device
 
 from ..tools.logger import Logger
 from .thermal import ThermalModel
-
-"""
-
 
 """
 Module Overview:
@@ -52,17 +39,12 @@ Usage Guide:
 
 """
 
-
-
-
 @dataclass
 class ControlModes:
     """
     Control modes for the Dephy Actpack.
 
     Available modes are Voltage, Current, Position, Impedance.
-    
-    ## Remain Unchanged
     """
 
     voltage: ctypes.c_int = fxe.FX_VOLTAGE
@@ -86,8 +68,6 @@ class Gains:
         K (int): Stiffness of the impedance controller
         B (int): Damping of the impedance controller
         ff (int): Feedforward gain
-        
-    ## Remain structure Unchanged, values change through actuator def class
     """
 
     kp: int = 0
@@ -100,14 +80,6 @@ class Gains:
     def __repr__(self) -> str:
         return f"kp={self.kp}, ki={self.ki}, kd={self.kd}, K={self.K}, B={self.B}, ff={self.ff}"
 
-
-"""
-Section for control parameters definition, needs to be packed into base class
-"""
-class MotorParam(ABC):
-    @abstractmethod
-    def __init__(self) -> None:
-        super().__init__()
 
 CONTROL_MODE = ControlModes()
 
@@ -134,11 +106,300 @@ DEFAULT_CURRENT_GAINS = Gains(kp=40, ki=400, kd=0, K=0, B=0, ff=128)
 
 DEFAULT_IMPEDANCE_GAINS = Gains(kp=40, ki=400, kd=0, K=200, B=400, ff=128)
 
-class MotorObj(ABC):
+
+class ActpackMode:
     """
-    Abstract Base Class for Motor Object
+    Base class for Actpack modes
+
+    Args:
+        control_mode (c_int): Control mode
+        device (DephyActpack): Dephy Actpack
     """
-    @abstractmethod
+
+    def __init__(self, control_mode: c_int, device: "DephyActpack") -> None:
+
+        self._control_mode: c_int = control_mode
+        self._device: DephyActpack = device
+        self._entry_callback: Callable[[], None] = lambda: None
+        self._exit_callback: Callable[[], None] = lambda: None
+
+        self._has_gains = False
+
+    def __eq__(self, __o: object) -> bool:
+        if isinstance(__o, ActpackMode):
+            return self._control_mode == __o._control_mode
+        return False
+
+    def __str__(self) -> str:
+        return str(object=self._control_mode)
+
+    def __repr__(self) -> str:
+        return f"ActpackMode[{self._control_mode}]"
+
+    @property
+    def mode(self) -> c_int:
+        """
+        Control mode
+
+        Returns:
+            c_int: Control mode
+        """
+        return self._control_mode
+
+    @property
+    def has_gains(self) -> bool:
+        """
+        Whether the mode has gains
+
+        Returns:
+            bool: True if the mode has gains, False otherwise
+        """
+        return self._has_gains
+
+    def enter(self) -> None:
+        """
+        Calls the entry callback
+        """
+        self._entry_callback()
+
+    def exit(self) -> None:
+        """
+        Calls the exit callback
+        """
+        self._exit_callback()
+
+    def transition(self, to_state: "ActpackMode") -> None:
+        """
+        Transition to another mode. Calls the exit callback of the current mode
+        and the entry callback of the new mode.
+
+        Args:
+            to_state (ActpackMode): Mode to transition to
+        """
+        self.exit()
+        to_state.enter()
+
+    def _set_voltage(self, voltage: int) -> None:
+        """
+        This method should be implemented by the child class. It should set the q axis voltage.
+        """
+        pass
+
+    def _set_current(self, current: int) -> None:
+        """
+        This method should be implemented by the child class. It should set the q axis current.
+        """
+        pass
+
+    def _set_motor_position(self, counts: int) -> None:
+        """
+        This method should be implemented by the child class. It should set the motor position.
+        """
+        pass
+
+
+class VoltageMode(ActpackMode):
+    def __init__(self, device: "DephyActpack") -> None:
+        super().__init__(control_mode=CONTROL_MODE.voltage, device=device)
+        self._entry_callback = self._entry
+        self._exit_callback = self._exit
+
+    def _entry(self) -> None:
+        self._device._log.debug(msg=f"[Actpack] Entering Voltage mode.")
+
+    def _exit(self) -> None:
+        self._device._log.debug(msg=f"[Actpack] Exiting Voltage mode.")
+        self._set_voltage(voltage=0)
+        time.sleep(0.1)
+
+    def _set_voltage(self, voltage: int) -> None:
+        self._device.send_motor_command(
+            ctrl_mode=self.mode,
+            value=voltage,
+        )
+
+
+class CurrentMode(ActpackMode):
+    def __init__(self, device: "DephyActpack") -> None:
+        super().__init__(control_mode=CONTROL_MODE.current, device=device)
+        self._entry_callback = self._entry
+        self._exit_callback = self._exit
+
+    def _entry(self) -> None:
+        self._device._log.debug(msg=f"[Actpack] Entering Current mode.")
+
+        if not self.has_gains:
+            self._set_gains()
+
+        self._set_current(current=0)
+
+    def _exit(self) -> None:
+        self._device._log.debug(msg=f"[Actpack] Exiting Current mode.")
+        self._device.send_motor_command(ctrl_mode=CONTROL_MODE.voltage, value=0)
+        time.sleep(1 / self._device.frequency)
+
+    def _set_gains(
+        self,
+        kp: int = DEFAULT_CURRENT_GAINS.kp,
+        ki: int = DEFAULT_CURRENT_GAINS.ki,
+        ff: int = DEFAULT_CURRENT_GAINS.ff,
+    ) -> None:
+
+        assert 0 <= kp <= 80, "kp must be between 0 and 80"
+        assert 0 <= ki <= 800, "ki must be between 0 and 800"
+        assert 0 <= ff <= 128, "ff must be between 0 and 128"
+
+        self._device.set_gains(kp=kp, ki=ki, kd=0, k=0, b=0, ff=ff)
+        self._has_gains = True
+
+    def _set_current(self, current: int) -> None:
+        """Sets the Q-axis current of the motor
+
+        Args:
+            current (int): _description_
+        """
+        self._device.send_motor_command(
+            ctrl_mode=self.mode,
+            value=current,
+        )
+
+
+class PositionMode(ActpackMode):
+    def __init__(self, device: "DephyActpack") -> None:
+        super().__init__(control_mode=CONTROL_MODE.position, device=device)
+        self._entry_callback = self._entry
+        self._exit_callback = self._exit
+
+    def _entry(self) -> None:
+        self._device._log.debug(msg=f"[Actpack] Entering Position mode.")
+
+        if not self.has_gains:
+            self._set_gains()
+
+        self._set_motor_position(
+            counts=int(self._device.motor_position / RAD_PER_COUNT)
+        )
+
+    def _exit(self) -> None:
+        self._device._log.debug(msg=f"[Actpack] Exiting Position mode.")
+        self._device.send_motor_command(ctrl_mode=CONTROL_MODE.voltage, value=0)
+        time.sleep(0.1)
+
+    def _set_gains(
+        self,
+        kp: int = DEFAULT_POSITION_GAINS.kp,
+        ki: int = DEFAULT_POSITION_GAINS.ki,
+        kd: int = DEFAULT_POSITION_GAINS.kd,
+        ff: int = DEFAULT_POSITION_GAINS.ff,
+    ) -> None:
+
+        assert 0 <= kp <= 1000, "kp must be between 0 and 1000"
+        assert 0 <= ki <= 1000, "ki must be between 0 and 1000"
+        assert 0 <= kd <= 1000, "kd must be between 0 and 1000"
+
+        self._device.set_gains(kp=kp, ki=ki, kd=kd, k=0, b=0, ff=ff)
+        self._has_gains = True
+
+    def _set_motor_position(self, counts: int) -> None:
+        """Sets the motor position
+
+        Args:
+            counts (int): position in counts
+        """
+        self._device.send_motor_command(
+            ctrl_mode=self.mode,
+            value=counts,
+        )
+
+
+class ImpedanceMode(ActpackMode):
+    def __init__(self, device: "DephyActpack") -> None:
+        super().__init__(control_mode=CONTROL_MODE.impedance, device=device)
+        self._entry_callback = self._entry
+        self._exit_callback = self._exit
+
+    def _entry(self) -> None:
+        self._device._log.debug(msg=f"[Actpack] Entering Impedance mode.")
+        if not self.has_gains:
+            self._set_gains()
+
+        self._set_motor_position(
+            counts=int(self._device.motor_position / RAD_PER_COUNT)
+        )
+
+    def _exit(self) -> None:
+        self._device._log.debug(msg=f"[Actpack] Exiting Impedance mode.")
+        self._device.send_motor_command(ctrl_mode=CONTROL_MODE.voltage, value=0)
+        time.sleep(1 / self._device.frequency)
+
+    def _set_motor_position(self, counts: int) -> None:
+        """Sets the motor position
+
+        Args:
+            counts (int): position in counts
+        """
+        self._device.send_motor_command(
+            ctrl_mode=self.mode,
+            value=counts,
+        )
+
+    def _set_gains(
+        self,
+        kp: int = DEFAULT_IMPEDANCE_GAINS.kp,
+        ki: int = DEFAULT_IMPEDANCE_GAINS.ki,
+        K: int = DEFAULT_IMPEDANCE_GAINS.K,
+        B: int = DEFAULT_IMPEDANCE_GAINS.B,
+        ff: int = DEFAULT_IMPEDANCE_GAINS.ff,
+    ) -> None:
+
+        assert 0 <= kp <= 80, "kp must be between 0 and 80"
+        assert 0 <= ki <= 800, "ki must be between 0 and 800"
+        assert 0 <= ff <= 128, "ff must be between 0 and 128"
+        assert 0 <= K, "K must be greater than 0"
+        assert 0 <= B, "B must be greater than 0"
+
+        self._device.set_gains(
+            kp=int(kp), ki=int(ki), kd=int(0), k=int(K), b=int(B), ff=int(ff)
+        )
+        self._has_gains = True
+
+
+@dataclass(init=False)
+class ActpackControlModes:
+    """
+    Actpack modes
+
+    Args:
+        voltage (VoltageMode): Voltage mode
+        current (CurrentMode): Current mode
+        position (PositionMode): Position mode
+        impedance (ImpedanceMode): Impedance mode
+    """
+
+    voltage: VoltageMode
+    current: CurrentMode
+    position: PositionMode
+    impedance: ImpedanceMode
+
+    def __init__(self, device: "DephyActpack") -> None:
+        self.voltage = VoltageMode(device=device)
+        self.current = CurrentMode(device=device)
+        self.position = PositionMode(device=device)
+        self.impedance = ImpedanceMode(device=device)
+
+    def __repr__(self) -> str:
+        return f"ActpackControlModes"
+
+class MotorABC:
+    """
+    Abstract Base Class for Motor Definition
+    """
     def __init__(self) -> None:
-        super().__init__()
-    
+        pass
+
+class MotorObj:
+    """
+    Object Definition for the Motor unit
+    """
+    def __init__(self) -> None:
+        pass
