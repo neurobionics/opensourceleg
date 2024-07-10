d0b4180c4e0613003a351a65f2c63dd7a83b6e58
Yuanshao Yang <yuanshao@umich.edu>
2024-07-10 20:58:06 +0100

    actuators tutorials updated

---
+++ tutorials/actuators/position_control.py
@@ -0,0 +1,34 @@
+﻿import opensourceleg.actuators.dephy as Dephy
+from opensourceleg.logging.logger import LOGGER
+import time
+import numpy as np
+
+actpack = Dephy.DephyActpack(
+    port="/dev/ttyACM0", 
+    gear_ratio=9.0, 
+)
+
+try:
+    actpack.start()
+    actpack.set_control_mode(mode = actpack.CONTROL_MODES.POSITION)
+    actpack.update()
+    current_position = actpack.output_position
+    while True: 
+        actpack.set_position_gains(
+            # if no input, then default gains are applied
+        )
+        actpack.set_output_position(
+            value = current_position + np.pi/2
+        )
+        actpack.update()
+        current_position = actpack.output_position
+        LOGGER.info("".join(
+              f"Motor Position: {actpack.output_position}\t"
+            + f"Motor Voltage: {actpack.motor_voltage}\t"
+            + f"Motor Current: {actpack.motor_current}\t"
+            )
+        )
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    actpack.stop()
---
+++ tutorials/joints/voltage_control.py
@@ -0,0 +1,25 @@
+﻿import opensourceleg.actuators.dephy as Dephy
+from opensourceleg.logging.logger import LOGGER
+import time
+
+actpack = Dephy.DephyActpack(
+    port="/dev/ttyACM0", 
+    gear_ratio=9.0, 
+)
+
+with actpack:
+    try:
+        actpack.set_control_mode(mode = actpack.CONTROL_MODES.VOLTAGE)
+        while True: 
+            actpack.set_motor_voltage(value = 2000)             # in mV
+            actpack.update()
+            LOGGER.info("".join(
+                f"Motor Position: {actpack.motor_position}\t"
+                + f"Motor Voltage: {actpack.motor_voltage}\t"
+                + f"Motor Current: {actpack.motor_current}\t"
+                )
+            )
+            time.sleep(0.1)
+
+    except KeyboardInterrupt:
+        exit()
