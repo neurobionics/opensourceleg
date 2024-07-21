f4a803b29422d0c31f4530c50b81404bd72f3fc1
Yuanshao Yang <yuanshao@umich.edu>
2024-07-21 21:51:46 +0100

    moteus driver test version finished, communication work but failed on actuator

--- opensourceleg/actuators/moteus.py
+++ opensourceleg/actuators/moteus.py
@@ -8,7 +8,7 @@ import os
 import time
 from dataclasses import dataclass
 import time
-
+import numpy as np
 from moteus import (
     Controller, 
     Command, 
@@ -44,10 +44,14 @@ from opensourceleg.logging.decorators import (
     deprecated_with_suggestion,
 )
 
+from opensourceleg.math import ThermalModel
+
+from opensourceleg.safety import ThermalLimitException
+
 from opensourceleg.logging.logger import LOGGER
 
 
-DEFAULT_POSITION_GAINS = ControlGains(kp=0, ki=0, kd=0, k=0, b=0, ff=0)
+DEFAULT_POSITION_GAINS = ControlGains(kp=50, ki=10, kd=20, k=0, b=0, ff=0)
 
 DEFAULT_CURRENT_GAINS = ControlGains(kp=0, ki=0, kd=0, k=0, b=0, ff=0)
 
@@ -59,7 +63,14 @@ class MoteusQueryResolution:
     position = mp.F32
     velocity = mp.F32
     torque = mp.F32
-    q_current = mp.IGNORE
+    
+    voltage = mp.INT8
+    temperature = mp.INT32
+    fault = mp.INT8
+
+
+    # Kept for Controller init reference
+    q_current = mp.F32
     d_current = mp.IGNORE
     abs_position = mp.IGNORE
     power = mp.IGNORE
@@ -67,15 +78,15 @@ class MoteusQueryResolution:
     trajectory_complete = mp.IGNORE
     rezero_state = mp.IGNORE
     home_state = mp.IGNORE
-    voltage = mp.INT8
-    temperature = mp.INT8
-    fault = mp.INT8
 
     aux1_gpio = mp.IGNORE
     aux2_gpio = mp.IGNORE
 
     _extra = {
-        # add custom registers here
+        MoteusRegister.COMMAND_KP_SCALE: mp.F32,
+        MoteusRegister.COMMAND_ILIMIT_SCALE: mp.F32,
+        MoteusRegister.COMMAND_KD_SCALE: mp.F32,
+        MoteusRegister.COMMAND_POSITION: mp.F32,
     }
 
 
@@ -109,7 +120,7 @@ class MoteusVoltageMode(ControlModeBase):
 
     def set_voltage(self, value: Union[float, int]):
         self._actuator._command = self._actuator.make_vfoc(
-            theta = 0, 
+            theta = 5.0, 
             voltage = value, 
             query = True,
         )
@@ -203,7 +214,7 @@ class MoteusPositionMode(ControlModeBase):
         if not self.has_gains:
             self.set_gains()
 
-        self.set_position(value=self.actuator.motor_position)
+        self.set_position(value=0)
 
     def _exit(self) -> None:
         LOGGER.debug(msg=f"[MoteusControlMode] Exiting {self.name} mode.")
@@ -217,18 +228,14 @@ class MoteusPositionMode(ControlModeBase):
         gains: ControlGains = DEFAULT_POSITION_GAINS,
     ) -> None:
         super().set_gains(gains)
-        # self.actuator.set_gains(
-        #     kp=int(gains.kp),
-        #     ki=int(gains.ki),
-        #     kd=int(gains.kd),
-        #     k=0,
-        #     b=0,
-        #     ff=int(gains.ff),
-        # )
 
     def set_position(self, value: Union[float, int]):
         self._actuator._command = self._actuator.make_position(
-            position = value, 
+            # feedforward_torque = self._gains.ff,
+            kp_scale = self._gains.kp, 
+            kd_scale = self._gains.kd,
+            ilimit_scale = self._gains.ki,
+            position = value, # in revolutions
             query = True,
         )
     def set_current(self, value: Union[float, int]):
@@ -277,7 +284,7 @@ class MoteusInterface:
     def __repr__(self):
         return f"MoteusInterface"
         
-    def add2map(self, servo_id, bus_id) -> None:
+    def _add2map(self, servo_id, bus_id) -> None:
         
         if bus_id in self.bus_map.keys():
             self.bus_map[bus_id].append(servo_id)
@@ -294,26 +301,25 @@ class MoteusInterface:
             )
         
     def update(self):
-        
-        # self._data = await self.transport.cycle(
-        #     self._commands
-        # )
+        # TODO: multiple servo syncing process should go here
         pass
         
         self._commands = []
     
     def stop(self):
+        # TODO: multiple servo syncing process should go here
         pass
 
 class MoteusController(ActuatorBase, Controller):
     def __init__(
         self,
         tag: str = "Moteus",
-        servo_id: int = 1,
-        bus_id: int = 11,
+        servo_id: int = 0,
+        bus_id: int = 0,
         gear_ratio: float = 1.0,
         frequency: int = 500,
         offline: bool = False,
+        query: MoteusQueryResolution = MoteusQueryResolution(),
     ) -> None:
         self._servo_id = servo_id
         self._bus_id = bus_id
@@ -329,30 +335,41 @@ class MoteusController(ActuatorBase, Controller):
         )
         
         self._interface = MoteusInterface()
-        self._interface.add2map(servo_id=servo_id, bus_id=bus_id)
+        self._interface._add2map(servo_id=servo_id, bus_id=bus_id)
         
         if self.is_offline: 
             self.is_streaming: bool = False
             self.is_open: bool = False
         else:
-            
+            # for streaming check
             self.is_streaming: bool = True
             self.is_open: bool = True
-            
-            pass
+        
+        self._command: Command = None
+        self._data = None
+        self._query = query
         
         self._encoder_map = None
 
         self._motor_zero_position = 0.0
         self._joint_zero_position = 0.0
+        self._motor_position_offset = 0.0
+
+        self._is_homed: bool = False
 
         self._joint_offset = 0.0
-        self._motor_offset = 0.0
+        self._motor_position_offset = 0.0
         self._joint_direction = 1.0
         
-        self._command: Command = None
-        self._data = None
-
+        self._thermal_model: ThermalModel = ThermalModel(
+            temp_limit_windings=self.max_winding_temperature,
+            soft_border_C_windings=10,
+            temp_limit_case=self.max_case_temperature,
+            soft_border_C_case=10,
+        )
+        self._thermal_scale: float = 1.0
+        
+        
 
     def __repr__(self) -> str:
         return f"Moteus[{self._actuator_name}]"
@@ -366,7 +383,7 @@ class MoteusController(ActuatorBase, Controller):
                 self, 
                 id = self._servo_id, 
                 transport=self._interface.transport, 
-                query_resolution=MoteusQueryResolution(),
+                query_resolution=self._query,
             )
         except OSError as e:
             print("\n")
@@ -375,14 +392,17 @@ class MoteusController(ActuatorBase, Controller):
             )
             os._exit(status=1)
 
-        # self._command = None
-        self._mode.enter()
+        self.mode.enter()
 
     @check_actuator_stream
     @check_actuator_open
     def stop(self) -> None:
         super().stop()
-        # self.set_motor_voltage(value=0)
+        self._data = self._interface.transport.cycle(
+            [self.make_stop(
+                query = True
+            )]
+        )
         self._command = None
         
 
@@ -392,24 +412,56 @@ class MoteusController(ActuatorBase, Controller):
         self._data = await self._interface.transport.cycle(
             [self._command]
         )
+        self._thermal_model.T_c = self.case_temperature
+        self._thermal_scale = self._thermal_model.update_and_get_scale(
+            dt = 1/self.frequency,
+            motor_current = self.motor_current,
+        )
+        if self.case_temperature >= self.max_case_temperature:
+                self._log.error(
+                    msg=f"[{str.upper(self._name)}] Case thermal limit {self.max_case_temperature} reached. Stopping motor."
+                )
+                raise ThermalLimitException()
+
+        if self.winding_temperature >= self.max_winding_temperature:
+            self._log.error(
+                msg=f"[{str.upper(self._name)}] Winding thermal limit {self.max_winding_temperature} reached. Stopping motor."
+            )
+            raise ThermalLimitException()
         
     def home(self): 
+        # To be continued ... 
         pass
     
     
     def set_control_mode(self, mode: ControlModeBase) -> None:
         super().set_control_mode(mode)
 
+    def set_motor_torque(self, value: float) -> None:
+        """
+        Sets the motor torque in Nm.
+
+        Args:
+            value (float): The torque to set in Nm.
+        """
+        self.set_motor_current(
+            value / self.MOTOR_CONSTANTS.NM_PER_MILLIAMP,
+        )
+
+    def set_joint_torque(self, value: float) -> None:
+        """
+        Set the joint torque of the joint.
+        This is the torque that is applied to the joint, not the motor.
+
+        Args:
+            value (float): torque in N_m
+        """
+        self.set_motor_torque(value=value / self.gear_ratio)
 
     def set_motor_current(
         self,
         value: float,
     ):
-        # self._command = self.make_current(
-        #     d_A = value, 
-        #     q_A=0, 
-        #     query = True, 
-        # )
         self.mode.set_current(
             value=value
         )
@@ -439,10 +491,7 @@ class MoteusController(ActuatorBase, Controller):
             position (float): The position to set
         """
         self.mode.set_position(
-            int(
-                (value + self.motor_zero_position + self.motor_offset)
-                / self.MOTOR_CONSTANTS.RAD_PER_COUNT
-            ),
+            value = value / (2*np.pi), 
         )
         # self._command = self.make_position(
         #     position = value, 
@@ -490,7 +539,7 @@ class MoteusController(ActuatorBase, Controller):
         kd: int = DEFAULT_IMPEDANCE_GAINS.kd,
         ff: int = DEFAULT_IMPEDANCE_GAINS.ff,
     ) -> None: 
-        pass
+        LOGGER.info(msg=f"[MoteusControlMode] Impedance mode not applicable.")
     
     def set_encoder_map(self, encoder_map) -> None:
         """Sets the joint encoder map"""
@@ -500,9 +549,9 @@ class MoteusController(ActuatorBase, Controller):
         """Sets motor zero position in radians"""
         self._motor_zero_position = position
 
-    def set_motor_offset(self, position: float) -> None:
+    def set_motor_position_offset(self, position: float) -> None:
         """Sets joint offset position in radians"""
-        self._motor_offset = position
+        self._motor_position_offset = position
 
     def set_joint_zero_position(self, position: float) -> None:
         """Sets joint zero position in radians"""
@@ -515,70 +564,170 @@ class MoteusController(ActuatorBase, Controller):
     def set_joint_direction(self, direction: float) -> None:
         """Sets joint direction to 1 or -1"""
         self._joint_direction = direction
-        
-    @property
-    def CONTROL_MODES(self) -> ControlModesBase:
-        """Control Modes (Read Only)
-
-        Returns:
-            List[ControlModeBase]: List of control modes
-        """
-        return self._CONTROL_MODES
 
     @property
-    def MOTOR_CONSTANTS(self) -> MotorConstants:
-        """Motor Constants (Read Only)
-
-        Returns:
-            MotorConstants: Motor Constants
-        """
-        return self._MOTOR_CONSTANTS
+    def joint_zero_position(self) -> float:
+        """Joint encoder zero position in radians."""
+        return self._joint_zero_position
 
     @property
-    def mode(self) -> ControlModeBase:
-        """Current Control Mode (Read Only)
-
-        Returns:
-            ControlModeBase: Current control mode
-        """
-        return self._mode
+    def joint_position_offset(self) -> float:
+        """Joint encoder offset in radians."""
+        return self._joint_position_offset
 
     @property
-    def actuator_name(self) -> str:
-        """Actuator Name (Read Only)
-
-        Returns:
-            str: Actuator name
-        """
-        return self._tag
+    def joint_direction(self) -> float:
+        """Joint direction: 1 or -1"""
+        return self._joint_direction
 
     @property
-    def frequency(self) -> int:
-        """Frequency (Read Only)
-
-        Returns:
-            int: Frequency
-        """
-        return self._frequency
+    def encoder_map(self):
+        """Polynomial coefficients defining the joint encoder map from counts to radians."""
+        return self._encoder_map
 
     @property
-    def is_offline(self) -> bool:
-        """Offline (Read Only)
-
-        Returns:
-            bool: Offline
-        """
-        return self._is_offline
+    def motor_zero_position(self) -> float:
+        """Motor encoder zero position in radians."""
+        return self._motor_zero_position
 
     @property
-    def gear_ratio(self) -> float:
-        """Gear Ratio (Read Only)
+    def motor_position_offset(self) -> float:
+        """Motor encoder offset in radians."""
+        return self._motor_position_offset
 
-        Returns:
-            float: Gear Ratio
+    @property
+    def motor_voltage(self) -> float:
+        """Q-axis motor voltage in mV."""
+        if self._data is not None:
+            return float(self._data[0].values[MoteusRegister.VOLTAGE])
+        else:
+            LOGGER.warning(
+                msg="Actuator data is none, please ensure that the actuator is connected and streaming. Returning 0.0."
+            )
+            return 0.0
+
+    @property
+    def motor_current(self) -> float:
+        if self._data is not None:
+            return float(self._data[0].values[MoteusRegister.Q_CURRENT])
+        else:
+            LOGGER.warning(
+                msg="Actuator data is none, please ensure that the actuator is connected and streaming. Returning 0.0."
+            )
+            return 0.0
+
+    @property
+    def motor_torque(self) -> float:
+        if self._data is not None:
+            return float(self.motor_current * self.MOTOR_CONSTANTS.NM_PER_MILLIAMP)
+        else:
+            LOGGER.warning(
+                msg="Actuator data is none, please ensure that the actuator is connected and streaming. Returning 0.0."
+            )
+            return 0.0
+
+    @property
+    def motor_position(self) -> float:
+        if self._data is not None:
+            return (
+                float(self._data[0].values[MoteusRegister.POSITION] * 2 * np.pi)
+                - self.motor_zero_position
+                - self.motor_position_offset
+            )
+        else:
+            LOGGER.warning(
+                msg="Actuator data is none, please ensure that the actuator is connected and streaming. Returning 0.0."
+            )
+            return 0.0
+
+    @property
+    def motor_velocity(self) -> float:
+        if self._data is not None:
+            return int(self._data[0].values[MoteusRegister.VELOCITY]) * 2 * np.pi
+        else:
+            LOGGER.warning(
+                msg="Actuator data is none, please ensure that the actuator is connected and streaming. Returning 0.0."
+            )
+            return 0.0
+
+    @property
+    def battery_voltage(self) -> float:
+        """Battery voltage in mV."""
+        if self._data is not None:
+            return float(self._data[0].values[MoteusRegister.VOLTAGE])
+        else:
+            LOGGER.warning(
+                msg="Actuator data is none, please ensure that the actuator is connected and streaming. Returning 0.0."
+            )
+            return 0.0
+
+    @property
+    def battery_current(self) -> float:
+        """Battery current in mA."""
+        if self._data is not None:
+            return float(self._data[0].values[MoteusRegister.Q_CURRENT])
+        else:
+            LOGGER.warning(
+                msg="Actuator data is none, please ensure that the actuator is connected and streaming. Returning 0.0."
+            )
+            return 0.0
+
+    @property
+    def output_position(self) -> float:
         """
-        return self._gear_ratio
+        Position of the output in radians.
+        This is calculated by scaling the motor angle with the gear ratio.
+        Note that this method does not consider compliance from an SEA.
+        """
+        return self.motor_position / self.gear_ratio
 
+    @property
+    def output_velocity(self) -> float:
+        """
+        Velocity of the output in radians.
+        This is calculated by scaling the motor angle with the gear ratio.
+        Note that this method does not consider compliance from an SEA.
+        """
+        return self.motor_velocity / self.gear_ratio
+
+    @property
+    def joint_torque(self) -> float:
+        """
+        Torque at the joint output in Nm.
+        This is calculated using motor current, k_t, and the gear ratio.
+        """
+        return self.motor_torque * self.gear_ratio
+
+    @property
+    def case_temperature(self) -> float:
+        if self._data is not None:
+            return float(self._data[0].values[MoteusRegister.TEMPERATURE])
+        else:
+            LOGGER.warning(
+                msg="Actuator data is none, please ensure that the actuator is connected and streaming. Returning 0.0."
+            )
+            return 0.0
+
+    
+    @property
+    def winding_temperature(self) -> float:
+        """
+        ESTIMATED temperature of the windings in celsius.
+        This is calculated based on the thermal model using motor current.
+        """
+        if self._data is not None:
+            return float(self._thermal_model.T_w)
+        else:
+            return 0.0
+
+    @property
+    def thermal_scaling_factor(self) -> float:
+        """
+        Scale factor to use in torque control, in [0,1].
+        If you scale the torque command by this factor, the motor temperature will never exceed max allowable temperature.
+        For a proof, see paper referenced in thermal model.
+        """
+        return self._thermal_scale
 
 if __name__ == "__main__":
     pass
