6825cffa6e713832895a7935c707458903d6ceb4
Senthur <imsenthur@gmail.com>
2024-08-22 19:24:52 -0400

    Added better unittests.

--- opensourceleg/actuators/base.py
+++ opensourceleg/actuators/base.py
@@ -23,21 +23,23 @@ import numpy as np
 from opensourceleg.logging.logger import LOGGER
 
 
-class MOTOR_CONSTANTS(NamedTuple):
-    # TODO: Add thermal constants
+@dataclass
+class MOTOR_CONSTANTS:
     MOTOR_COUNT_PER_REV: float
     NM_PER_AMP: float
     NM_PER_RAD_TO_K: float
     NM_S_PER_RAD_TO_B: float
-
     MAX_CASE_TEMPERATURE: float
     MAX_WINDING_TEMPERATURE: float
 
+    def __post_init__(self):
+        if any(x <= 0 for x in self.__dict__.values()):
+            raise ValueError(
+                "All values in MOTOR_CONSTANTS must be non-zero and positive."
+            )
+
     @property
     def RAD_PER_COUNT(self) -> float:
-        if self.MOTOR_COUNT_PER_REV == 0:
-            return 0
-
         return 2 * np.pi / self.MOTOR_COUNT_PER_REV
 
     @property
