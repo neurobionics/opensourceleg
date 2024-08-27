5deb140fd90235d8fcf6288c6016fc2078427094
Senthur <imsenthur@gmail.com>
2024-08-27 18:27:51 -0400

    Revert "Moved logger module description."
    This reverts commit 915ae79ce230d07ead1ba760f513e5455da5c0b0.

--- opensourceleg/logging/__init__.py
+++ opensourceleg/logging/__init__.py
@@ -1,1 +1,27 @@
 from opensourceleg.logging.logger import *
+
+"""
+Logging module for opensourceleg library.
+
+Module Overview:
+
+This module defines a custom logger class, `Logger`, designed to log attributes
+from class instances to a CSV file. It extends the `logging.Logger` class.
+
+Key Classes:
+
+- `LogLevel`: Enum class that defines the log levels supported by the `Logger` class.
+- `Logger`: Logs attributes of class instances to a CSV file. It supports
+setting different logging levels for file and stream handlers.
+- `LOGGER`: Global instance of the `Logger` class that can be used throughout
+
+Usage Guide:
+
+1. Create an instance of the `Logger` class.
+2. Optionally, set the logging levels for file and stream handlers using
+   `set_file_level` and `set_stream_level` methods.
+3. Add class instances and attributes to log using the `track_variable` method.
+4. Start logging data using the `update` method.
+5. PLEASE call the `close` method before exiting the program to ensure all data is written to the log file.
+
+"""
