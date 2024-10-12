4d44df9bf9487c77f991deaf69c9bb97fb850b60
esharnow <esharnow@umich.edu>
2024-10-12 19:08:51 -0400

    added tests for safety module, deleted utlities module, improved functionality of safety.py

--- opensourceleg/utilities/__init__.py
+++
@@ -1,9 +0,0 @@
-from opensourceleg.utilities.utilities import *
-
-"""
-Utilities module for opensourceleg library.
-
-Module Overview:
-
-This module provides utility functions for the opensourceleg library.
-"""
