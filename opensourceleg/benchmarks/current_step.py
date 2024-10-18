e3d4792779409dce6cc77f4d5af21b784e6d5949
andrebe <andrea.berettoni1996@gmail.com>
2024-10-18 11:47:33 -0400

    Consolidated futek and added argparse

--- opensourceleg/benchmarks/current_step.py
+++ opensourceleg/benchmarks/current_step.py
@@ -8,22 +8,36 @@ from opensourceleg.sensors.torque_sensor import Futek
 from opensourceleg.benchmarks.benchmark_manager import SimpleTimer, LimitVelocity
 import numpy as np
 import csv
+import argparse
 
 FREQUENCY = 200
 
 
 
 def current_step():
-    t_cond = 2.0 # [sec], time held on target
-    current_velocity = 500 # [mA/s], ramp current velocity
-    max_current = 4000 # mA
-    num_steps = 21
+    parser = argparse.ArgumentParser()
+    parser.add_argument("time", type=float, help="time held on each current target [sec]")
+    parser.add_argument("current_velocity", type=int,  help="ramp current velocity [mA/s]")
+    parser.add_argument("max_current", type=int, help="time held on each current target [sec]")
+    parser.add_argument("num_steps", type=int, help="number of current targets")
+
+    args = parser.parse_args()
+
+    # t_cond = 2.0 # [sec], time held on target
+    # current_velocity = 500 # [mA/s], ramp current velocity
+    # max_current = 4000 # mA
+    # num_steps = 21
+
+    t_cond = args.time # [sec], time held on target
+    current_velocity = args.current_velocity # [mA/s], ramp current velocity
+    max_current = args.max_current # mA
+    num_steps = args.num_steps
 
     # current_steps = np.linspace(0, max_current, num=num_steps)
     current_steps = [(i / (num_steps - 1)) * max_current for i in range(num_steps)]
 
     futek = Futek()
-    futek.calibrate_loadcell()
+    futek.calibrate()
 
     on_target = 0
     curr_command = 0.0
@@ -51,8 +65,11 @@ def current_step():
 
             driving_motor.update()
 
-            if timer.is_done:
-                if timer.just_done:
+            is_done = timer.is_done
+            just_done = timer.just_done
+
+            if is_done:
+                if just_done:
                     if current_steps is not None:
                         curr_command = current_steps.pop(0)
                         print("Setting q-current to %2f mA."% curr_command)
@@ -73,11 +90,11 @@ def current_step():
 
             current = driving_motor.motor_current
             output_position = driving_motor.output_position
-            futek.get_torque()
+            futek.update()
 
-            print("Desired Current: ", curr_command_lim, "Current: ", current, "Futek Torque: ", futek.torque)
+            print("Desired Current: ", curr_command_lim, "Current: ", current, "Futek Torque: ", futek.mx)
 
-            csv_writer.writerow([t, on_target, curr_command_lim, current, output_position, futek.torque])
+            csv_writer.writerow([t, on_target, curr_command_lim, current, output_position, futek.mx])
 
 if __name__ == "__main__":
     current_step()
--- opensourceleg/sensors/torque_sensor.py
+++ opensourceleg/sensors/torque_sensor.py
@@ -2,60 +2,66 @@ import busio
 import board
 import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
-import csv
-import traceback
-import sys
+# import csv
+# import traceback
+# import sys
 import numpy as np
 from time import time, sleep
 
-from opensourceleg.sensors.base import SensorBase
+from opensourceleg.sensors.base import LoadcellBase
 
-class AdcManager(object):
-    def __init__(self, csv_file_name=None):
-        self.i2c = busio.I2C(board.SCL, board.SDA)
-        self.ads = ADS.ADS1115(self.i2c, data_rate=860)
-        self.chan = AnalogIn(self.ads, ADS.P0)
-        self.save_csv = not (csv_file_name is None)
-        self.csv_file_name = csv_file_name
-        self.csv_file = None
-        self.csv_writer = None
-        self.volts = -42.0 # error code
-
-    def __enter__(self):
-        self.start()
-        if self.save_csv:
-            with open(self.csv_file_name,'w') as fd:
-                writer = csv.writer(fd)
-                writer.writerow(["pi_time", "voltage", "test_duration"])
-            self.csv_file = open(self.csv_file_name,'a').__enter__()
-            self.csv_writer = csv.writer(self.csv_file)
-        return self
-
-    def __exit__(self, etype, value, tb):
-        """ Closes the file properly """
-        self.stop()
-        if self.save_csv:
-            self.csv_file.__exit__(etype, value, tb)
-        if not (etype is None):
-            traceback.print_exception(etype, value, tb)
+# class AdcManager(object):
+#     def __init__(self, csv_file_name=None):
+#         self.i2c = busio.I2C(board.SCL, board.SDA)
+#         self.ads = ADS.ADS1115(self.i2c, data_rate=860)
+#         self.chan = AnalogIn(self.ads, ADS.P0)
+#         self.save_csv = not (csv_file_name is None)
+#         self.csv_file_name = csv_file_name
+#         self.csv_file = None
+#         self.csv_writer = None
+#         self.volts = -42.0 # error code
 
 
-    def update_adc(self):
-        t0=time()
-        self.volts = self.chan.voltage
-        dur = time()-t0
-        if self.save_csv:
-            self.csv_writer.writerow([time(),self.volts, dur])
-        return self.volts
+#     def __enter__(self):
+#         self.start()
+#         if self.save_csv:
+#             with open(self.csv_file_name,'w') as fd:
+#                 writer = csv.writer(fd)
+#                 writer.writerow(["pi_time", "voltage", "test_duration"])
+#             self.csv_file = open(self.csv_file_name,'a').__enter__()
+#             self.csv_writer = csv.writer(self.csv_file)
+#         return self
 
-class Futek(AdcManager, SensorBase):
-    def __init__(self, torque=-42.0, bias=0, torque_rating=100, wait_time=5, voltage=5, **kwargs):
-        super().__init__(**kwargs)
+#     def __exit__(self, etype, value, tb):
+#         """ Closes the file properly """
+#         self.stop()
+#         if self.save_csv:
+#             self.csv_file.__exit__(etype, value, tb)
+#         if not (etype is None):
+#             traceback.print_exception(etype, value, tb)
+
+
+#     def update_adc(self):
+#         t0=time()
+#         self.volts = self.chan.voltage
+#         dur = time()-t0
+#         if self.save_csv:
+#             self.csv_writer.writerow([time(),self.volts, dur])
+#         return self.volts
+
+class Futek(LoadcellBase):
+    def __init__(self, torque=-42.0, bias=0.0, torque_rating=100.0, wait_time=5.0, voltage=5.0, **kwargs):
         self.torque = torque
         self.bias = bias
         self.torque_rating = torque_rating
         self.wait_time = wait_time
         self.voltage = voltage
+        self.volts = -42.0 # error code
+        self.i2c = busio.I2C(board.SCL, board.SDA)
+        self.ads = ADS.ADS1115(self.i2c, data_rate=860)
+        self.chan = AnalogIn(self.ads, ADS.P0)
+        self._is_calibrated = False
+        self._is_streaming = False
 
     def start(self):
         self._is_streaming = True
@@ -64,15 +70,15 @@ class Futek(AdcManager, SensorBase):
         self._is_streaming = False
 
     def update(self):
-        self.get_torque()
-
-    def get_torque(self):
-        self.update_adc()
+        self.volts = self.chan.voltage
         torque_rating = self.torque_rating # 100 Nm = 5V
-        self.torque = (self.volts-self.voltage/2-self.bias)/self.voltage/2*torque_rating
-        return self.torque
+        self.torque = (self.volts-(self.voltage/2)-self.bias)/(self.voltage/2)*torque_rating
 
-    def calibrate_loadcell(self,Calibrate=True,SaveCal=False):
+    def reset(self):
+        self._is_calibrated = False
+
+
+    def calibrate(self,Calibrate=True,SaveCal=False):
         if Calibrate:
             self.update()
             voltage_cal = []
@@ -94,11 +100,45 @@ class Futek(AdcManager, SensorBase):
             print("Setting Bias: {} V".format(bias))
             self.bias = bias
 
+        self._is_calibrated = True
         return bias
+
     def set_bias(self,bias):
         self.bias = bias
         print("Bias set to: {} V".format(bias))
 
     @property
+    def mx(self):
+        return self.torque
+
+    @property
+    def my(self):
+        print("Method not implemented")
+
+    @property
+    def mz(self):
+        print("Method not implemented")
+
+    @property
+    def fx(self):
+        print("Method not implemented")
+
+    @property
+    def fy(self):
+        print("Method not implemented")
+
+    @property
+    def fz(self):
+        print("Method not implemented")
+
+    @property
+    def data(self):
+        print("Method not implemented")
+
+    @property
     def is_streaming(self) -> bool:
         return self._is_streaming
+
+    @property
+    def is_calibrated(self) -> bool:
+        return self._is_calibrated
