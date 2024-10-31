06c5928e1986fb16fe3da1403161d6af3e00fb93
esharnow <esharnow@umich.edu>
2024-10-31 17:11:59 -0400

    added tests for robots and sensors modules. improved logging tests. created try except blocks for imports in imu.py

--- tests/test_sensors/test_imu.py
+++ tests/test_sensors/test_imu.py
@@ -1,11 +1,33 @@
-from unittest.mock import Mock, patch
+from unittest.mock import Mock
 
 import pytest
 
-from opensourceleg.sensors.imu import *
 from opensourceleg.sensors.base import *
 from opensourceleg.logging import LOGGER
 
+
+class MockTypes:
+    CH_FIELD_ESTFILTER_ESTIMATED_ORIENT_EULER = "Estimated Orientation Euler"
+    CH_FIELD_ESTFILTER_ESTIMATED_ANGULAR_RATE = "Estimated Angular Rate"
+    CH_FIELD_ESTFILTER_ESTIMATED_LINEAR_ACCEL = "Estimated Linear Acceleration"
+    CH_FIELD_ESTFILTER_GPS_TIMESTAMP = "GPS Timestamp"
+
+
+@pytest.fixture
+def patch_mscl(monkeypatch):
+    mock_rate = "Sample Rate"
+    mock_channels = Mock()
+    mock_channel = Mock()
+
+    monkeypatch.setattr('mscl.MipChannels', lambda: mock_channels)
+    monkeypatch.setattr('mscl.MipChannel', lambda *args: mock_channel)
+    monkeypatch.setattr('mscl.SampleRate', mock_rate)
+    monkeypatch.setattr('mscl.MipTypes', MockTypes)
+
+    return mock_channel
+
+
+from opensourceleg.sensors.imu import *
 
 # Creating a Mock LordMicrostrainIMU Class
 class MockLordMicrostrainIMU(LordMicrostrainIMU): 
@@ -26,10 +48,24 @@ class MockLordMicrostrainIMU(LordMicrostrainIMU):
 def sample_imu():
     return MockLordMicrostrainIMU()
 
+# Test configure mip channels
+def test_configure_mip_channels(sample_imu: MockLordMicrostrainIMU, patch_mscl):
+    channels = sample_imu._configure_mip_channels()
+    
+    assert len(channels) == 4
+    
+    mock_channel = patch_mscl
+
+    mock_channel.assert_any_call(MockTypes.CH_FIELD_ESTFILTER_ESTIMATED_ORIENT_EULER, "Sample Rate")
+    mock_channel.assert_any_call(MockTypes.CH_FIELD_ESTFILTER_ESTIMATED_ANGULAR_RATE, "Sample Rate")
+    mock_channel.assert_any_call(MockTypes.CH_FIELD_ESTFILTER_ESTIMATED_LINEAR_ACCEL, "Sample Rate")
+    mock_channel.assert_any_call(MockTypes.CH_FIELD_ESTFILTER_GPS_TIMESTAMP, "Sample Rate")
+
+
+#PLACE BACK BEFORE OTHER TESTS
 @pytest.fixture
 def sample_imu_init():
     sample_imu = MockLordMicrostrainIMU()
-
 
     sample_imu._data = {"estRoll": [0], "estPitch": [0], 
                         "estYaw": [0], "estAngularRateX": [0], 
@@ -40,7 +76,7 @@ def sample_imu_init():
 
 
 # Test init
-def test_init_default(sample_imu: LordMicrostrainIMU):
+def test_init_default(sample_imu: MockLordMicrostrainIMU):
     assert all([sample_imu.port == "/dev/ttyUSB0", 
                sample_imu.baud_rate == 921600,
                sample_imu.frequency == 200,
@@ -65,45 +101,13 @@ def test_init_set():
                 sample_imu._baud_rate == baud,
                 sample_imu._frequency == freq])
 
-
-# Test configure mip channels
-def test_configure_mip_channels(monkeypatch, sample_imu: LordMicrostrainIMU):
-    class MockTypes:
-        CH_FIELD_ESTFILTER_ESTIMATED_ORIENT_EULER = "Estimated Orientation Euler"
-        CH_FIELD_ESTFILTER_ESTIMATED_ANGULAR_RATE = "Estimated Angular Rate"
-        CH_FIELD_ESTFILTER_ESTIMATED_LINEAR_ACCEL = "Estimated Linear Acceleration"
-        CH_FIELD_ESTFILTER_GPS_TIMESTAMP = "GPS Timestamp"
-    
-    mock_channels = Mock()
-    mock_channel = Mock()
-
-    mock_rate = "Mocked mscl Sample Rate"
-
-    monkeypatch.setattr('mscl.MipChannels', lambda: mock_channels)
-    monkeypatch.setattr('mscl.MipChannel', lambda *args: mock_channel)
-    monkeypatch.setattr('mscl.SampleRate', mock_rate)
-    monkeypatch.setattr('mscl.MipTypes', MockTypes)
-
-    channels = sample_imu._configure_mip_channels()
-    
-    assert len(channels) == 4
-    
-    mock_channels.assert_called_once()
-
-    mock_channel.assert_called_once_with(MockTypes.CH_FIELD_ESTFILTER_ESTIMATED_ORIENT_EULER, mock_rate)
-    mock_channel.assert_called_once_with(MockTypes.CH_FIELD_ESTFILTER_ESTIMATED_ANGULAR_RATE, mock_rate)
-    mock_channel.assert_called_once_with(MockTypes.CH_FIELD_ESTFILTER_ESTIMATED_LINEAR_ACCEL, mock_rate)
-    mock_channel.assert_called_once_with(MockTypes.CH_FIELD_ESTFILTER_GPS_TIMESTAMP, mock_rate)
-
-
-
 # Test start
-#def test_start(sample_imu: LordMicrostrainIMU):
+#def test_start(sample_imu: MockLordMicrostrainIMU):
     #fix
 
 
 # Test stop
-#def test_stop(sample_imu: LordMicrostrainIMU):
+#def test_stop(sample_imu: MockLordMicrostrainIMU):
     #sample_imu.start()
     #assert sample_imu.is_streaming == True
     #sample_imu.stop()
@@ -112,7 +116,7 @@ def test_configure_mip_channels(monkeypatch, sample_imu: LordMicrostrainIMU):
 
 
 # Test ping
-def test_ping(sample_imu: LordMicrostrainIMU):
+def test_ping(sample_imu: MockLordMicrostrainIMU):
     with pytest.raises(SensorNotStreamingException) as e:
         sample_imu.ping()
     assert (
@@ -123,107 +127,107 @@ def test_ping(sample_imu: LordMicrostrainIMU):
 
 
 # Test update
-#def test_update(sample_imu: LordMicrostrainIMU):
+#def test_update(sample_imu: MockLordMicrostrainIMU):
     #fix
 
 
 # Test LordMicrostrainIMU repr
-def test_lord_microstrain_imu_repr(sample_imu: LordMicrostrainIMU):
+def test_lord_microstrain_imu_repr(sample_imu: MockLordMicrostrainIMU):
     assert sample_imu.__repr__() == "IMULordMicrostrain"
 
 
 # Test port
-def test_port(sample_imu: LordMicrostrainIMU):
+def test_port(sample_imu: MockLordMicrostrainIMU):
     test = "test"
     sample_imu._port = test
     assert sample_imu.port == test
 
 
 # Test baud rate
-def test_baud_rate(sample_imu: LordMicrostrainIMU):
+def test_baud_rate(sample_imu: MockLordMicrostrainIMU):
     test = 10
     sample_imu._baud_rate = test
     assert sample_imu.baud_rate == test
 
 
 # Test frequency
-def test_frequency(sample_imu: LordMicrostrainIMU):
+def test_frequency(sample_imu: MockLordMicrostrainIMU):
     test = 10
     sample_imu._frequency = test
     assert sample_imu.frequency == test
 
 
 # Test is_streaming
-def test_is_streaming(sample_imu: LordMicrostrainIMU):
+def test_is_streaming(sample_imu: MockLordMicrostrainIMU):
     sample_imu._is_streaming = True
     assert sample_imu.is_streaming == True
 
 
 # Test roll
-def test_roll(sample_imu_init: LordMicrostrainIMU):
+def test_roll(sample_imu_init: MockLordMicrostrainIMU):
     estRoll = 5
     sample_imu_init._data["estRoll"] = estRoll
     assert sample_imu_init.roll == estRoll
 
 
 # Test pitch
-def test_pitch(sample_imu_init: LordMicrostrainIMU):
+def test_pitch(sample_imu_init: MockLordMicrostrainIMU):
     estPitch = 10
     sample_imu_init._data["estPitch"] = estPitch
     assert sample_imu_init.pitch == estPitch
 
 
 # Test yaw
-def test_yaw(sample_imu_init: LordMicrostrainIMU):
+def test_yaw(sample_imu_init: MockLordMicrostrainIMU):
     estYaw = 15
     sample_imu_init._data["estYaw"] = estYaw
     assert sample_imu_init.yaw == estYaw
 
 
 # Test vel_x
-def test_vel_x(sample_imu_init: LordMicrostrainIMU):
+def test_vel_x(sample_imu_init: MockLordMicrostrainIMU):
     estVelx = 20
     sample_imu_init._data["estAngularRateX"] = estVelx
     assert sample_imu_init.vel_x == estVelx
 
 
 # Test vel_y
-def test_vel_y(sample_imu_init: LordMicrostrainIMU):
+def test_vel_y(sample_imu_init: MockLordMicrostrainIMU):
     estVely = 25
     sample_imu_init._data["estAngularRateY"] = estVely
     assert sample_imu_init.vel_y == estVely
 
 
 # Test vel_z
-def test_vel_z(sample_imu_init: LordMicrostrainIMU):
+def test_vel_z(sample_imu_init: MockLordMicrostrainIMU):
     estVelz = 30
     sample_imu_init._data["estAngularRateZ"] = estVelz
     assert sample_imu_init.vel_z == estVelz
 
 
 # Test acc_x
-def test_acc_x(sample_imu_init: LordMicrostrainIMU):
+def test_acc_x(sample_imu_init: MockLordMicrostrainIMU):
     estAccx = 35
     sample_imu_init._data["estLinearAccelX"] = estAccx
     assert sample_imu_init.acc_x == estAccx
 
 
 # Test acc_y
-def test_acc_y(sample_imu_init: LordMicrostrainIMU):
+def test_acc_y(sample_imu_init: MockLordMicrostrainIMU):
     estAccy = 40
     sample_imu_init._data["estLinearAccelY"] = estAccy
     assert sample_imu_init.acc_y == estAccy
 
 
 # Test acc_z
-def test_acc_z(sample_imu_init: LordMicrostrainIMU):
+def test_acc_z(sample_imu_init: MockLordMicrostrainIMU):
     estAccz = 45
     sample_imu_init._data["estLinearAccelZ"] = estAccz
     assert sample_imu_init.acc_z == estAccz
 
 
 # Test timestamp
-def test_timestamp(sample_imu_init: LordMicrostrainIMU):
+def test_timestamp(sample_imu_init: MockLordMicrostrainIMU):
     estTime = 50
     sample_imu_init._data["estFilterGpsTimeTow"] = estTime
     assert sample_imu_init.timestamp == estTime
